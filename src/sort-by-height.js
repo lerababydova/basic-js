6cb9c2bb64102da9e4ae1df91adf95595d8fac84
unknown <babydova.2011@gmail.com>
2022-05-02 21:56:44 +0400

    complete 3 tasks

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function sortByHeight(arr) {
+  let indexes = [];
+  arr.forEach((e, index) => {
+    if (e === -1) {
+      indexes.push(index);
+    }
+  });
+  let sorted = arr
+    .filter((el) => !!~el)
+    .sort((a, b) => {
+      return a - b;
+    });
 
+  indexes.forEach((index) => {
+    sorted.splice(index, 0, -1);
+  });
+  return sorted;
+}
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,47 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+
+  try {
+    date.getTime();
+  } catch (p) {
+    throw new Error("Invalid date!");
+  }
+
+  if (!(date instanceof Date) || isNaN(date)) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getMonth() + 1;
+
+  if ((month >= 1 && month <= 2) || month === 12) {
+    return "winter";
+  }
+  if (month >= 3 && month <= 5) {
+    return "spring";
+  }
+  if (month >= 6 && month <= 8) {
+    return "summer";
+  }
+  if (month >= 9 && month <= 11) {
+    return "autumn";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
