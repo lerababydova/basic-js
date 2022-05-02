3d2a723486439702d8dbe5bf9d3ffc5d724bbf31
unknown <babydova.2011@gmail.com>
2022-05-02 18:20:42 +0400

    complete 2 tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const arrStrOne = s1.split("");
+  const arrStrTwo = s2.split("");
+  let counter = 0;
+
+  arrStrOne.forEach((e) => {
+    if (arrStrTwo.includes(e)) {
+      counter++;
+      arrStrTwo.splice(arrStrTwo.indexOf(e), 1);
+    }
+  });
+  return counter;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,63 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let array = [...arr];
+
+  array.forEach((el, index) => {
+    switch (el) {
+      case "--double-next":
+        if (index === array.length - 1) {
+          array.pop();
+        } else {
+          array[index] = array[index + 1];
+        }
+        break;
+      case "--double-prev":
+        if (index === 0) {
+          array.shift();
+        } else {
+          array[index] = array[index - 1];
+        }
+        break;
+      case "--discard-next":
+        array[index] = undefined;
+        array[index + 1] = undefined;
+        break;
+      case "--discard-prev":
+        if (index === 0) {
+          array.shift();
+        } else {
+          array.splice(index - 1, 2);
+        }
+        break;
+      default:
+        break;
+    }
+  });
+  return array.filter((el) => typeof el !== "undefined");
 }
 
+console.log(
+  transform([1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5])
+);
+
 module.exports = {
-  transform
+  transform,
 };
